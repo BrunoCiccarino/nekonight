2828b631dd7a77e4c5317422145a36f9e1c76b68
BrunoCiccarino <landneco0@gmail.com>
2024-12-26 17:34:43 -0300

    fix: fix gruvbox colors names

--- lua/nekonight/colors/gruvbox.lua
+++ lua/nekonight/colors/gruvbox.lua
@@ -1,4 +1,8 @@
 local ret = vim.deepcopy(require("nekonight.colors.storm"))
+local green3 = blue5 
+local pastelWhite = dark3 
+local yesun = green1
+local vibred = purple
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
@@ -10,23 +14,23 @@ return vim.tbl_deep_extend("force", ret, {
   blue0 = "#458588",
   blue1 = "#65bcff",
   blue2 = "#0db9d7",
-  blue5 = "#8ec07c",
+  green3 = "#8ec07c",
   blue6 = "#b4f9f8",
   blue7 = "#458588",
   comment = "#928374",
   cyan = "#86e1fc",
-  dark3 = "#f2e5bc",
+  pastelWhite = "#f2e5bc",
   dark5 = "#458588",
   fg = "#c8d3f5",
   fg_dark = "#828bb8",
   fg_gutter = "#f2e5bc",
   green = "#8ec07c",
-  green1 = "#d79921",
+  yesun = "#d79921",
   green2 = "#41a6b5",
   magenta = "#c099ff",
   magenta2 = "#ff007c",
   orange = "#ff966c",
-  purple = "#cc241d",
+  vibred = "#cc241d",
   red = "#ff757f",
   red1 = "#c53b53",
   teal = "#4fd6be",
