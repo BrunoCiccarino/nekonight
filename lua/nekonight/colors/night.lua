0d27b6acd2af5fdcdc7d791a2c66782a90d1e8c6
BrunoCiccarino <landneco0@gmail.com>
2025-01-02 22:33:20 -0300

    💄 Improving colorscheme

--- lua/nekonight/colors/night.lua
+++ lua/nekonight/colors/night.lua
@@ -2,7 +2,7 @@ local ret = vim.deepcopy(require("nekonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#231b2f",
+  bg = "#262835",
   bg_dark = "#1b1524",
   bg_dark1 = "#1b1524",
   bg_highlight = "#515c6a",
